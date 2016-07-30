d6c3e45b2c1ba212304d1efd05923880b1f5c110
madhav-datt <madhav.datt@hotmail.com>
2016-07-30 12:03:03 +0530

    website update test

--- join.php
+++ join.php
@@ -2,8 +2,8 @@
 $email = $_POST['email'];
 $city = $_POST['city'];
 $country = $_POST['country'];
-$to = "contribute@greenthegene.org";
-$subject = "JOIN IN FROM GreentheGene.org home page";
+$to = "greenthegene@gmail.com";
+$subject = "Sign up on greenthegene.org home page";
 
 mail ($to, $subject, $city, $country, "From:$email"  );
 include 'home.html'
